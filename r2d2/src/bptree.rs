cfe38137be28383fc082d6979f18c83b738f7ce2
Matthew Kenneth Peterson <97690958+MKP157@users.noreply.github.com>
2024-11-28 18:34:43 -0400

    Working leaf-nodes

--- r2d2/src/bptree.rs
+++ r2d2/src/bptree.rs
@@ -1,6 +1,10 @@
+use std::path::Component::ParentDir;
 use bson;
+use bson::Bson;
 
 pub struct LeafNode {
+    min_size: usize,
+    max_size: usize,
     keys: Vec<u128>,
     values: Vec<bson::Document>,
     // next_leaf may be null, need option
@@ -10,8 +14,12 @@ pub struct LeafNode {
 impl LeafNode {
     pub fn new(num_keys: usize) -> LeafNode {
         LeafNode {
-            keys: vec![0; num_keys],
-            values: vec![bson::doc!{}; num_keys],
+            // Create a vector of keys and a vector of BSON documents.
+            // Each BSON represents a row of the table.
+            min_size: num_keys.div_ceil(2) - 1,
+            max_size: num_keys,
+            keys: Vec::new(),
+            values: Vec::new(),
             next_leaf: None
         }
     }
@@ -19,31 +27,116 @@ impl LeafNode {
     pub fn print(self: &mut LeafNode) {
         print!("[");
         for i in 0..self.keys.len() {
-            print!("{} ", self.keys[i]);
+            print!("{} : {}, ", self.keys[i], self.values[i]);
         }
         print!("]");
     }
 
-    // TODO: find
-    // TODO: insert
-    // TODO: delete
-    // TODO: split_and_push
-    // TODO: combine_and_shrink
-}
+    // find:
+    // Returns index within node, if key is found.
+    pub fn find(self: &mut LeafNode, key: u128) -> Option<usize> {
+        for i in 0..self.keys.len() {
+            if self.keys[i] == key {
+                // Return index when found
+                return Some(i);
+            }
+        }
 
+        // No value if not found.
+        return None;
+    }
+
+    // insert:
+    // Insert key-value pair.
+    // If operation is successful, return 0.
+    // If node is full, return 1.
+    // If key already exists, return 2.
+    // If for some reason
+
+    pub fn insert(self: &mut LeafNode, key: u128, value: bson::Document) -> i32 {
+        // If node is full:
+        if self.keys.len() == self.max_size {
+            return 1;
+        }
+
+        else if self.keys.len() == 0 {
+            self.keys.push(key);
+            self.values.push(value);
+            return 0;
+        }
+
+        else {
+            let mut i = 0;
+            while i < (self.keys.len() - 1) {
+                if key == self.keys[i] {
+                    return 2;
+                }
+
+                else if key < self.keys[i] {
+                    break;
+                }
+
+                i += 1;
+            }
+
+            self.keys.insert(i, key);
+            self.values.insert(i, value);
+            return 0;
+        }
+    }
+
+
+    // remove:
+    // Return's a status code, and optionally the removed key-value pair if found.
+    // If operation successful, return 0.
+    // If key was found, but the resulting LeafNode after removing the key-value pair
+    //      would be smaller than half its maximum length, return 1.
+    // If key not found, return 2.
+    pub fn remove(self: &mut LeafNode, key: u128) -> (i32, Option<(u128, bson::Document)>) {
+        let found = self.find(key);
+        // If key exists in LeafNode:
+        if found.is_some() {
+            // If result would be too small:
+            if self.keys.len() == self.min_size {
+                return (1, None);
+            }
+
+            else {
+                let i = found.unwrap();
+                return (0, Some( (self.keys.remove(i), self.values.remove(i) )));
+            }
+        }
+
+        // If key not in LeafNode;
+        else {
+            return (2, None);
+        }
+    }
+
+    // TODO: Set next
+}
 
 
 pub struct BPTree {
     root: LeafNode,
-    num_keys: usize,
+    // Keys per node.
+    fan_out: usize,
+    // Details for each column.
+    // Column structure: {name:string, type:("int" | "string" | "double" | "bool")}
+    schema: bson::Document,
 }
 
 impl BPTree {
     // TODO: new
-    // TODO: insert
-    // TODO: delete
+    // TODO: insert key-value pair (includes split-and-push)
+    // TODO: delete key-value pair (includes combine-and-pull)
+
     // TODO: find
     // TODO: print
-    
+
+    // TODO: query
+    // TODO: serialize to file
+    // TODO: deserialize from file
+
 
 }
--- r2d2/src/main.rs
+++ r2d2/src/main.rs
@@ -1,5 +1,110 @@
+use bson::{Bson, Document};
+
 mod bptree;
 
+fn leaf_node_tests () {
+    println!("LeafNode tests...");
+    println!("=================");
+
+    let max : usize = 3;
+
+    let mut test_node = bptree::LeafNode::new(max);
+
+    let mut test_docs : Vec<Document> = Vec::new();
+    test_docs.push(bson::doc! {
+        "name" : "Matthew",
+        "age" : 21,
+        "student" : true
+    });
+
+    test_docs.push(bson::doc! {
+        "name" : "Dr. Kim",
+        "age" : 40,
+        "student" : false
+    });
+
+    test_docs.push(bson::doc! {
+        "name" : "Aiden",
+        "age" : 21,
+        "student" : true
+    });
+
+    println!("\n\n======== INSERT TESTS ========");
+
+    let mut status : i32;
+
+    // Insert normal tests
+    status = test_node.insert(3,  test_docs[0].clone());
+    println!("Insert completed with status {}, result:", status);
+    test_node.print();
+    print!("\n\n");
+
+    status = test_node.insert(1,  test_docs[1].clone());
+    println!("Insert completed with status {}, result:", status);
+    test_node.print();
+    print!("\n\n");
+
+    // Insert with collision
+    status = test_node.insert(1,  test_docs[2].clone());
+    println!("Insert completed with status {}, result:", status);
+    test_node.print();
+    print!("\n\n");
+
+    status = test_node.insert(2,  test_docs[2].clone());
+    println!("Insert completed with status {}, result:", status);
+    test_node.print();
+    print!("\n\n");
+
+    // Insert on full:
+    status = test_node.insert(4,  test_docs[2].clone());
+    println!("Insert completed with status {}, result:", status);
+    test_node.print();
+    print!("\n\n");
+
+
+    // Removal tests
+    println!("\n\n======== REMOVE TESTS ========");
+    let mut retrieved : Option<(u128, bson::Document)>;
+
+
+    // Nonexistent key
+    (status, retrieved) = test_node.remove(5);
+    println!("Remove completed with status {}, result:", status);
+    test_node.print();
+    if retrieved.is_some() {
+        let (k, d) = retrieved.unwrap();
+        print!("\nReturned: {} : {}", k, d);
+    }
+    print!("\n\n");
+
+    // Key exists
+    (status, retrieved) = test_node.remove(2);
+    test_node.print();
+    if retrieved.is_some() {
+        let (k, d) = retrieved.unwrap();
+        print!("\nReturned: {} : {}", k, d);
+    }
+    print!("\n\n");
+
+    // Less than half full
+    (status, retrieved) = test_node.remove(1);
+    test_node.print();
+    if retrieved.is_some() {
+        let (k, d) = retrieved.unwrap();
+        print!("\nReturned: {} : {}", k, d);
+    }
+    print!("\n\n");
+
+    (status, retrieved) = test_node.remove(3);
+    test_node.print();
+    if retrieved.is_some() {
+        let (k, d) = retrieved.unwrap();
+        print!("\nReturned: {} : {}", k, d);
+    }
+    print!("\n\n");
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    leaf_node_tests();
 }
